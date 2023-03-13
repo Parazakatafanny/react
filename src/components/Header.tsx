3b9004c0c05d8a4cd1a08e600a356877efce891b
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-03-13 15:03:51 +0300

    feat: create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from '../assets/cat.png'
+export default class Header extends React.Component {
+  render() {
+    return<>
+      <header className="header">
+        <div className="container">
+          <div className="header__inner">
+            <img className="header__logo" src={logo} alt="cat" draggable="false" />
+            <div className="header__inner-links">
+              <a href="#" className="header__inner-link active-link">main</a>
+              <a href="#" className="header__inner-link">about us</a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>;
+  }
+}
