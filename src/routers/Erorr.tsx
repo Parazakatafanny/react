87947ef66ea5397b326ca96dfccf193ab6e532ec
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-03-15 15:50:11 +0300

    feat: create error page

--- src/routers/Erorr.tsx
+++ src/routers/Erorr.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import cat from '../assets/cat.png';
 
 export default function ErrorPage() {
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>jopa</p>
-      <p>paje not found</p>
+    <div className="error-page">
+      <h1 className="error-page__title">ERROR 404</h1>
+      <p className="error-page__text">page not found</p>
+      <img className="error-page__img" src={cat} alt="cat" />
     </div>
   );
 }
