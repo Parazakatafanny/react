2d0f64d736b6ee4d441ae241a9c393085e5e495c
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-03-16 12:05:22 +0300

    feat: implement search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 
-export default class Search extends React.Component {
+type Props = object;
+type State = {
+  inputValue: string;
+};
+
+export default class Search extends React.Component<Props, State> {
+  constructor(props: object) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  componentDidMount() {
+    const value = localStorage.getItem('search__input') || '';
+    this.setState({ inputValue: value });
+  }
+
+  componentWillUnmount() {
+    const { inputValue } = this.state;
+    localStorage.setItem('search__input', inputValue);
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ inputValue: event.target.value });
+  };
+
   render() {
+    const { inputValue } = this.state;
+
     return (
       <div className="search">
         <div className="container">
           <div className="search__inner">
-            <input className="search__input" type="text" />
+            <input
+              value={inputValue}
+              onChange={this.handleChange}
+              className="search__input"
+              type="text"
+            />
             <button className="search__button" type="button">
               search
             </button>
