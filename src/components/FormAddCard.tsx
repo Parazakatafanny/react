4b1a5c8b2c955026aa2590388bdc668593ff5e2f
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-03-23 23:11:05 +0300

    fix: change components to uncontrolled

--- src/components/FormAddCard.tsx
+++ src/components/FormAddCard.tsx
@@ -1,79 +1,70 @@
 import React from 'react';
 
-type Props = object;
-type State = {
-  name: string;
-  birthday: string;
-  pet: string;
-  scales: boolean;
-  horns: boolean;
-  tail: boolean;
-  ears: boolean;
-  fangs: boolean;
-  gender: string;
-  img: string;
-};
+export default class FormAddCards extends React.Component {
+  private formLink: React.RefObject<HTMLFormElement>;
 
-export default class FormAddCards extends React.Component<Props, State> {
+  private name: React.RefObject<HTMLInputElement>;
+
+  private birthday: React.RefObject<HTMLInputElement>;
+
+  private pet: React.RefObject<HTMLSelectElement>;
+
+  private scales: React.RefObject<HTMLInputElement>;
+
+  private img: React.RefObject<HTMLInputElement>;
+
+  private horns: React.RefObject<HTMLInputElement>;
+
+  private tail: React.RefObject<HTMLInputElement>;
+
+  private ears: React.RefObject<HTMLInputElement>;
+
+  private fangs: React.RefObject<HTMLInputElement>;
+
+  private gender: React.RefObject<HTMLInputElement>;
+
   constructor(props: object) {
     super(props);
-    this.state = {
-      name: '',
-      birthday: '',
-      pet: '',
-      scales: false,
-      horns: false,
-      tail: false,
-      ears: false,
-      fangs: false,
-      gender: '',
-      img: '',
-    };
 
-    this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formLink = React.createRef();
+    this.name = React.createRef();
+    this.birthday = React.createRef();
+    this.pet = React.createRef();
+    this.scales = React.createRef();
+    this.horns = React.createRef();
+    this.tail = React.createRef();
+    this.ears = React.createRef();
+    this.fangs = React.createRef();
+    this.gender = React.createRef();
+    this.img = React.createRef();
   }
 
-  handleChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>): void {
-    const { target } = event;
-    let value;
-    if (target.type === 'checkbox') {
-      value = (target as HTMLInputElement).checked;
-    } else if (target.type === 'file') {
-      const file = (target as HTMLInputElement).files![0];
-      value = window.URL.createObjectURL(file);
-    } else {
-      value = target.value;
-    }
+  handleSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log(
+      `A name was submitted: ${this.name.current?.value}, ${this.pet.current?.value}, ${this.birthday.current?.value}`
+    );
 
-    const { name } = target;
-    this.setState({ [name]: value } as Pick<State, keyof State>);
-    console.log(this.state);
+    this.formLink.current?.reset();
+    event.preventDefault();
   }
 
   render() {
-    const { name, birthday, pet, scales, horns, tail, ears, fangs, gender } = this.state;
     return (
-      <form className="form">
+      <form className="form" ref={this.formLink}>
         <label className="form__label" htmlFor="name">
           name
-          <input id="name" type="text" name="name" value={name} onChange={this.handleChange} />
+          <input id="name" type="text" name="name" ref={this.name} />
         </label>
 
         <label className="form__label" htmlFor="birthday">
           birthday
-          <input
-            id="birthday"
-            type="date"
-            name="birthday"
-            value={birthday}
-            onChange={this.handleChange}
-          />
+          <input id="birthday" type="date" name="birthday" ref={this.birthday} />
         </label>
 
         <label className="form__label" htmlFor="pet">
           choose a pet:
-          <select id="pet" name="pet" value={pet} onChange={this.handleChange}>
+          <select id="pet" name="pet" ref={this.pet}>
             <option value="dog">Dog</option>
             <option value="cat">Cat</option>
             <option value="hamster">Hamster</option>
@@ -87,53 +78,23 @@ export default class FormAddCards extends React.Component<Props, State> {
           Choose your pet features:
           <label htmlFor="scales">
             scales
-            <input
-              type="checkbox"
-              id="scales"
-              name="scales"
-              checked={scales}
-              onChange={this.handleChange}
-            />
+            <input type="checkbox" id="scales" name="scales" ref={this.scales} />
           </label>
           <label htmlFor="horns">
             horns
-            <input
-              type="checkbox"
-              id="horns"
-              name="horns"
-              checked={horns}
-              onChange={this.handleChange}
-            />
+            <input type="checkbox" id="horns" name="horns" ref={this.horns} />
           </label>
           <label htmlFor="tail">
             tail
-            <input
-              type="checkbox"
-              id="tail"
-              name="tail"
-              checked={tail}
-              onChange={this.handleChange}
-            />
+            <input type="checkbox" id="tail" name="tail" ref={this.tail} />
           </label>
           <label htmlFor="ears">
             ears
-            <input
-              type="checkbox"
-              id="ears"
-              name="ears"
-              checked={ears}
-              onChange={this.handleChange}
-            />
+            <input type="checkbox" id="ears" name="ears" ref={this.ears} />
           </label>
           <label htmlFor="fangs">
             fangs
-            <input
-              type="checkbox"
-              id="fangs"
-              name="fangs"
-              checked={fangs}
-              onChange={this.handleChange}
-            />
+            <input type="checkbox" id="fangs" name="fangs" ref={this.fangs} />
           </label>
         </div>
 
@@ -141,37 +102,17 @@ export default class FormAddCards extends React.Component<Props, State> {
           Select a gender of the pet:
           <label htmlFor="male">
             male
-            <input
-              type="radio"
-              id="male"
-              name="gender"
-              value="male"
-              checked={gender === 'male'}
-              onChange={this.handleChange}
-            />
+            <input type="radio" id="male" name="gender" value="male" ref={this.gender} />
           </label>
           <label htmlFor="female">
             female
-            <input
-              type="radio"
-              id="female"
-              name="gender"
-              value="female"
-              checked={gender === 'female'}
-              onChange={this.handleChange}
-            />
+            <input type="radio" id="female" name="gender" value="female" ref={this.gender} />
           </label>
         </fieldset>
 
-        <input
-          type="file"
-          name="img"
-          onChange={this.handleChange}
-          id="pet-img"
-          accept="image/png, image/jpeg"
-        />
+        <input type="file" name="img" id="pet-img" accept="image/png, image/jpeg" ref={this.img} />
 
-        <button type="button" className="form__btn">
+        <button type="button" onClick={this.handleSubmit} className="form__btn">
           CREATE
         </button>
       </form>
