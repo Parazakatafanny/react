5c15531264cb410b9964c7aaf0b25045a9995d09
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-03-24 18:00:24 +0300

    feat: configure coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'jsdom',
+    coverage: {
+      provider: 'c8',
+      all: true,
+      skipFull: false,
+      reporter: 'text',
+    },
   },
 });
