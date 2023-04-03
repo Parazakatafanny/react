e0353a570c19a467dd394fc12e5c92d465b56177
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-04-03 14:44:17 +0300

    feat: generate new api cards

--- src/routers/Main.tsx
+++ src/routers/Main.tsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/Card';
 import Search from '../components/Search';
-import cards from '../data/cards';
+// import cards from '../data/cards';
+
+const URL = 'https://rickandmortyapi.com/api';
+
+export interface CardType {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+}
 
 export default function Main() {
-  const cardsList: JSX.Element[] = [];
-  cards.forEach((card) => {
-    cardsList.push(
-      <Card
-        key={card.id}
-        rating={card.rating}
-        likes={card.likes}
-        description={card.description}
-        img={card.thumbnail}
-        title={card.title}
-      />
-    );
-  });
+  const [cardsList, setcardsList] = useState<CardType[]>([]);
+
+  useEffect(() => {
+    fetch(`${URL}/character`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((cards) => {
+        setcardsList(cards.results);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <>
       <Search />
       <div className="cards">
         <div className="container">
-          <div className="cards__inner">{cardsList}</div>
+          <div className="cards__inner">
+            {cardsList.map((card) => (
+              <Card
+                key={card.id}
+                name={card.name}
+                gender={card.gender}
+                species={card.species}
+                img={card.image}
+                status={card.status}
+                type={card.type}
+                planet={card.origin.name}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
