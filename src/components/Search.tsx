ec4ff48d0c61142fd363ffabf86dbf137926c394
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-04-04 15:41:03 +0300

    fix: change the search operation to the correct one

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Search() {
   const [inputValue, setInputValue] = useState<string>(localStorage.getItem('search__input') || '');
 
+  const currentValue = useRef(inputValue);
+
   useEffect(() => {
-    localStorage.setItem('search__input', inputValue);
-  }, [inputValue, setInputValue]);
+    return () => {
+      localStorage.setItem('search__input', currentValue.current);
+    };
+  }, []);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setInputValue(event.target.value);
+    const { value } = event.target;
+    setInputValue(value);
+    currentValue.current = value;
   };
 
   return (
